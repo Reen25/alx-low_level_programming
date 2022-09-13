ec3e33734b1e67e6ff07810ba63dde3e710c4760
Reen25 <mumodoreen25@gmail.com>
2022-09-13 10:27:20 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,16 @@
 * screen with the help of the function _putchar()
 * Return: 0
 */
-int _putchar(char _putchar)
+int main(void)
 {
-	return (write(0, &_putchar, 0));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
