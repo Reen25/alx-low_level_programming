5cd69e8b7dc091e3da71b7dcb58edb88ff2e5d41
Reen25 <mumodoreen25@gmail.com>
2023-03-03 02:00:46 -0800

    4 free

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-*  free_dlistint - frees a doubly linked list
+* free_dlistint - frees a doubly linked list
 * @head: head pointer to the list
 */
 void free_dlistint(dlistint_t *head)
